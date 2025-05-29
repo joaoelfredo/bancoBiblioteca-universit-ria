34b96394f269f6452aee11162bba3b140f8a094b
joaoelfredo <elfredojoao@gmail.com>
2025-05-29 19:39:01 -0300

    Add files via upload

---
+++ xuxapark.sql
@@ -0,0 +1,14 @@
+USE biblioteca_universitaria;
+INSERT INTO categoria (id, nome_categoria) VALUES (1, 'Literatura');
+INSERT INTO categoria (id, nome_categoria) VALUES (2, 'Ciências');
+INSERT INTO categoria (id, nome_categoria) VALUES (3, 'Engenharia');
+INSERT INTO categoria (id, nome_categoria) VALUES (4, 'História');
+INSERT INTO categoria (id, nome_categoria) VALUES (5, 'Matemática');
+INSERT INTO categoria (id, nome_categoria) VALUES (6, 'Filosofia');
+INSERT INTO categoria (id, nome_categoria) VALUES (7, 'Informática');
+INSERT INTO categoria (id, nome_categoria) VALUES (8, 'Biologia');
+
+INSERT INTO tipodeobra (id, nome) VALUES (1, 'Livro');
+INSERT INTO tipodeobra (id, nome) VALUES (2, 'Revista');
+INSERT INTO tipodeobra (id, nome) VALUES (3, 'Artigo');
+INSERT INTO tipodeobra (id, nome) VALUES (4, 'Monografia');
